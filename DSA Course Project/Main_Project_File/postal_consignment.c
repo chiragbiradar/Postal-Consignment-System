7f26901337e1b8af364b1876e7a5b828136dda38
Chirag Biradar <78417411+chiragbiradar@users.noreply.github.com>
2022-01-05 19:07:07 +0530

    Update postal_consignment.c

--- DSA Course Project/Main_Project_File/postal_consignment.c
+++ DSA Course Project/Main_Project_File/postal_consignment.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include<time.h>
+#include<limits.h>
+#include<stdbool.h>
+
 
 
 
@@ -23,6 +26,7 @@ struct city_info
 struct post_info
 {
     int speed_rating;
+    char departure_place[20],destination_place[20];
 };
 
 
@@ -32,11 +36,83 @@ int printRandoms_o(int lower, int upper,int count)
 {
 	int i;
 	for (i = 0; i < count; i++) {
-		int num = (rand() %
-		(upper - lower + 1)) + lower;
+		int num = (rand() %(upper - lower + 1)) + lower;
 		return num;
 	}
 
+}
+
+
+
+
+int minDistance(int dist[],bool sptSet[])
+{
+	
+	// Initialize min value
+	int min = INT_MAX, min_index;
+
+	for (int v = 0; v < NOC; v++)
+		if (sptSet[v] == false && dist[v] <= min)
+			min = dist[v], min_index = v;
+
+	return min_index;
+}
+
+void printPath(int parent[], int j)
+{
+	
+	// Base Case : If j is source
+	if (parent[j] == - 1)
+		return;
+
+	printPath(parent, parent[j]);
+
+	printf("%d-> ", j);
+}
+
+int printSolution(int dist[], int n,int parent[],int dest)
+{
+	int src = 0;
+	printf("Vertex\t Distance\tPath");
+	//for (int i = 1; i < V; i++)
+	//{
+		printf("\n%d -> %d \t\t %d\t\t%d-> ",src, dest, dist[dest], src);
+		printPath(parent, dest);
+	//}
+}
+
+void dijkstra(int graph[NOC][NOC], int src,int dest)
+{
+	int dist[NOC];
+
+	bool sptSet[NOC];
+
+	int parent[NOC];
+
+	for (int i = 0; i < NOC; i++)
+	{
+		parent[0] = -1;
+		dist[i] = INT_MAX;
+		sptSet[i] = false;
+	}
+
+	dist[src] = 0;
+
+	for (int count = 0; count < NOC - 1; count++)
+	{
+		int u = minDistance(dist, sptSet);
+		sptSet[u] = true;
+		for (int v = 0; v < NOC; v++)
+			if (!sptSet[v] && graph[u][v] && dist[u] + graph[u][v] < dist[v])
+			{
+				parent[v] = u;
+				dist[v] = dist[u] + graph[u][v];
+			}
+	}
+
+	// print the constructed
+	// distance array
+	printSolution(dist, NOC, parent,dest);
 }
 
 
@@ -62,7 +138,7 @@ int main()
         printf("%d  %s\n",i,ci[i].name);
     }*/
 
-    FILE *fptr = fopen("distance_graph.txt", "r");
+    FILE *fptr = fopen("graph_distance.txt", "r");
     if (fptr == NULL)
     {
         printf("Error Reading File!!!\n");
@@ -73,7 +149,7 @@ int main()
     {
         for (int j = 0; j < 100; j++)
         {
-            fscanf(fptr, "%d", graph[k][j]);
+            fscanf(fptr, "%d", &graph[k][j]);
         }
     }
     fclose(fptr);
@@ -81,9 +157,17 @@ int main()
 
 srand(time(0));
 int no_of_deliveries=printRandoms_0(20, 100, 1);
+struct post_info PI[no_of_deliveries];
+for(int start=0;start<no_of_deliveries;start++)
+{
+        PI[start]..
+    
 
 
 
+dijkstra(graph,)
+
+}
 
     return 0;
 }
